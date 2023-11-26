b6b4ceee15c1a77d549e0621a6110edc73c9d0cf
cinnamon0703 <147472099+cinnamon0703@users.noreply.github.com>
2023-11-26 20:45:46 +0900

    아잇 실수
    멍청하게 index에 attractor.js를 추가 안했다...

--- 1st experiment/sketch.js
+++ 1st experiment/sketch.js
@@ -4,10 +4,13 @@ let emitter;
 //{!1} One repeller
 let repeller;
 
+let attractor;
+
 function setup() {
   createCanvas(640, 240);
   emitter = new Emitter(width / 2, 20);
   repeller = new Repeller(width / 2, 200);
+  attractor = new Attractor(width/2, 100);
 }
 
 function draw() {
@@ -18,6 +21,7 @@ function draw() {
   emitter.applyForce(gravity);
   //{!1} Applying the repeller
   emitter.applyRepeller(repeller);
+  emitter.applyAttractor(attractor);
   emitter.run();
 
   repeller.show();
