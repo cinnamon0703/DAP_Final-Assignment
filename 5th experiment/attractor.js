48befa090d2a20faf79ef96f09a20e99901bf3a2
cinnamon0703 <147472099+cinnamon0703@users.noreply.github.com>
2023-11-27 21:40:01 +0900

    기본 틀 만들기

---
+++ 5th experiment/attractor.js
@@ -0,0 +1,25 @@
+class Attractor {
+    constructor(x, y) {
+      this.position = createVector(x, y);
+    
+      this.power = 500;
+    }
+  
+    show() {
+      stroke(0);
+      strokeWeight(2);
+      fill(255,0,0);
+      circle(this.position.x, this.position.y, 40);
+    }
+  
+    attract(particle) {
+      //{!6 .code-wide} This is the same repel algorithm we used in Chapter 2: forces based on gravitational attraction.
+      let force = p5.Vector.sub(this.position, particle.position);
+      let distance = force.mag();
+      distance = constrain(distance, 5, 50);
+      let strength = (1 * this.power) / (distance * distance);
+      force.setMag(strength);
+      return force;
+    }
+  }
+  
---
+++ 5th experiment/sketch.js
@@ -0,0 +1,27 @@
+// One ParticleSystem
+let emitter;
+
+//{!1} One repeller
+
+let attractor;
+
+
+function setup() {
+  createCanvas(1000, 1000);
+  emitter = new Emitter(width / 2, 50);
+  attractor = new Attractor(width/2, height/2);
+ 
+
+}
+
+function draw() {
+  background(0);
+  emitter.addParticle();
+  // We’re applying a universal gravity.
+  let gravity = createVector(0, 0.1);
+  emitter.applyForce(gravity);
+  //{!1} Applying the repeller
+  emitter.applyAttractor(attractor);
+  emitter.run();
+  attractor.show();
+}
