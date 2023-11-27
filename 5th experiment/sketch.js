3bdcace20a9911b377bda0a7d3021f662513431b
cinnamon0703 <147472099+cinnamon0703@users.noreply.github.com>
2023-11-27 22:03:37 +0900

    attractor도 무작위로 배치

--- 5th experiment/sketch.js
+++ 5th experiment/sketch.js
@@ -1,15 +1,17 @@
 // One ParticleSystem
 let emitters = [];
-let attractor;
+let attractors = [];
 
 function setup() {
   createCanvas(1000, 1000);
   
   for (let i = 0; i < 20; i++) {
-    emitters.push(new Emitter(random(width), 50));
+    emitters.push(new Emitter(random(width), random(height)));
   }
 
-  attractor = new Attractor(width/2, height/2);
+  for (let i = 0; i < 5; i++) {
+    attractors.push(new Attractor(random(width), random(height)));
+  }
 }
 
 function draw() {
@@ -19,9 +21,16 @@ function draw() {
     emitters[i].addParticle();
     let gravity = createVector(0, 0.1);
     emitters[i].applyForce(gravity);
-    emitters[i].applyAttractor(attractor);
+    
+    // Apply attractors
+    for (let j = 0; j < attractors.length; j++) {
+      emitters[i].applyAttractor(attractors[j]);
+    }
+
     emitters[i].run();
   }
 
-  attractor.show();
+  for (let i = 0; i < attractors.length; i++) {
+    attractors[i].show();
+  }
 }
