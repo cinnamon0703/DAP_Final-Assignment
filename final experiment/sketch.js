9034644e28fd28e75674958f137b6257d7e4f16e
cinnamon0703 <147472099+cinnamon0703@users.noreply.github.com>
2023-12-15 15:06:28 +0900

    gui를 움직이더라도 값이 랜덤하게 나오게 해보자
    그런데 이게 아니네...? 뭐지?

--- final experiment/sketch.js
+++ final experiment/sketch.js
@@ -1,22 +1,22 @@
 let tValue = 1;
 let tValueMin = 0;
 let tValueMax = 3000;
-let tValueStep = 1;
+let tValueStep = random(-10, 10);
 let tAmount = 1;
 let tAmountMin = 0;
 let tAmountMax = 10;
-let tAmountStep = 1;
+let tAmountStep = random(-1, 1);
 let gui;
 
 let params = {
   testValue : 1,
   testValueMin : 0,
   testValueMax : 3000,
-  testValueStep : 1,
+  testValueStep : random(-10, 10),
   testAmount : 1,
   testAmountMin : 0,
   testAmountMax : 10,
-  testAmountStep : 1,
+  testAmountStep : random(-1, 1),
   tColor : [200, 0, 0],
 }
 
